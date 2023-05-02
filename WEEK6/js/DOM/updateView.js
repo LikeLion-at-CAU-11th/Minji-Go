72bcc316aa7ee71946a7f1abeef8bfb58f8d17e6
gominzip <ahaminji@naver.com>
2023-05-02 09:59:35 +0900

    Docs : updateView 추가 모듈화

--- WEEK6/js/DOM/updateView.js
+++ WEEK6/js/DOM/updateView.js
@@ -2,19 +2,28 @@
 const gameResult = document.getElementById("result");
 const gameWinner = document.getElementById("racing-winners");
 
-export function gameView(carList, gameCount){
-    // 실행결과
-    for(let i=0; i<gameCount; i++){
-        const newBr = document.createElement('br');
-        gameResult.appendChild(newBr);
-        carList.cars.forEach((car) => {
-            car.updateScore();
-            const newDiv = document.createElement('div');
-            const newText = document.createTextNode(`${car.name}: ${car.scoreStr}`);
-            newDiv.appendChild(newText);
-            gameResult.appendChild(newDiv);
-        });
-    }
-    // 최종 우승자
+// 실행결과 DOM
+function gameResultView(carList) {
+    carList.cars.forEach((car) =>{
+        car.updateScore();
+        const newDiv = document.createElement('div');
+        const newText = document.createTextNode(`${car.name}: ${car.scoreStr}`);
+        newDiv.appendChild(newText);
+        gameResult.appendChild(newDiv);
+    });
+
+    const newBr = document.createElement('br');
+    gameResult.appendChild(newBr);
+}
+// 우승자 DOM
+function gameWinnerView(carList) {
     gameWinner.innerText = carList.getWinner();
 }
+
+export function gameView(carList, gameCount) {
+    for (let i = 0; i < gameCount; i++){
+        gameResultView(carList);
+    }
+    gameWinnerView(carList);
+}
+
