6ab5b3df7deb51cf258f5049202c4f78597401d2
gominzip <ahaminji@naver.com>
2023-07-06 18:12:26 +0900

    [FEAT] routing 구현코드 추가

--- week12/src/App.js
+++ week12/src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import Mypage from "./pages/Mypage";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/signup" element={<SignUp/>}></Route>
+        <Route path="/mypage" element={<Mypage/>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
