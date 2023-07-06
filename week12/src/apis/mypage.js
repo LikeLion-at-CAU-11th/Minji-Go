147a6a5ac20d163d12703f8c8a0ff1f9f8e2edb5
gominzip <ahaminji@naver.com>
2023-07-06 19:45:50 +0900

    [FEAT] 유저 데이터 화면출력 추가

--- week12/src/apis/mypage.js
+++ week12/src/apis/mypage.js
@@ -1,11 +1,11 @@
-import axios from "axios"
+import axios from "axios";
 // 데이터 요청
-export const getMyPage =async()=>{
-    const access = localStorage.getItem('access');  // 로컬스토리지에서 access 토큰을 가져온다
-    const result = await axios.get('http://front.cau-likelion.org/mypage',{
-        headers:{
-            Authorization: access,
-        },
-    });
-    return result.data
-}
+export const getMyPage = async () => {
+  const access = localStorage.getItem("access"); // 로컬스토리지에서 access 토큰을 가져온다
+  const result = await axios.get("http://front.cau-likelion.org/mypage", {
+    headers: {
+      Authorization: access,
+    },
+  });
+  return result.data;
+};
--- week12/src/pages/Mypage.js
+++ week12/src/pages/Mypage.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { getMyPage } from "../apis/mypage";
 
 const Mypage = () => {
+  const [data, setData] = useState(); // access토큰으로 가져온 data
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    getMyPage().then((res) => {
+      setData(res);
+      setLoading(false);
+    });
+  }, []);
+  if (loading) return <div>로딩중 ...</div>;
   return (
-    <div>Mypage</div>
-  )
-}
+    <div>
+      {/* 옵셔널체이닝 */}
+      <div>{data?.name}</div>
+      <div>{data?.age}</div>
+    </div>
+  );
+};
 
-export default Mypage
+export default Mypage;
