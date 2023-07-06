16db66815432eb36c0a644070c562c934dab5066
gominzip <ahaminji@naver.com>
2023-07-06 18:35:44 +0900

    [FEAT] id, pw의 useState 훅 추가

--- week12/src/pages/Home.js
+++ week12/src/pages/Home.js
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Input, Inputs, Title, Wrapper } from "../components/Common";
 import { styled } from "styled-components";
 import { Link } from "react-router-dom";
 
 const Home = () => {
+  const [id, setId] = useState("");
+  const [pw, setPW] = useState("");
+  const onChangeId = (e) => {
+    setId(e.target.value);
+  };
+  const onChangePW = (e) => {
+    setPW(e.target.value);
+  };
+
+  const onClick = () => {
+    //로그인 api
+  }
   return (
     <Wrapper>
       <Title>로그인하기</Title>
       <Form>
         <Inputs>
-          <Input placeholder="아이디" />
-          <Input placeholder="비밀번호" />
+          <Input placeholder="아이디" value={id} onChange={onChangeId} />
+          <Input
+            placeholder="비밀번호"
+            type="password"
+            value={pw}
+            onChange={onChangePW}
+          />
         </Inputs>
         <Button>Login</Button>
       </Form>
