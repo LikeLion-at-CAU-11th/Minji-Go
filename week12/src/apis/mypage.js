4b48d0cf79f2e076b860ee11164d7227cbdb7d62
gominzip <ahaminji@naver.com>
2023-07-09 15:56:57 +0900

    [REFACTOR] Axios Instance를 이용해 코드 단순화

--- week12/src/apis/mypage.js
+++ week12/src/apis/mypage.js
@@ -1,23 +1,8 @@
-import axios from "axios";
-import { getNewRefreshToken } from "./refresh";
+import { getAuthAxios } from "./authAxios";
 // 데이터 요청
 export const getMyPage = async () => {
   const access = localStorage.getItem("access"); // 로컬스토리지에서 access 토큰을 가져온다
-  try {
-    const result = await axios.get("http://front.cau-likelion.org/mypage", {
-      headers: {
-        Authorization: access,
-      },
-    });
-    return result.data;
-  } catch (error) {
-    if (error.response.status === 401) {
-      // 토큰이 만료된 경우
-      const { accessToken, refreshToken } = await getNewRefreshToken();
-      error.config.headers.Authorization = accessToken;
-      localStorage.setItem("access", accessToken);
-      localStorage.setItem("refresh", refreshToken);
-      return (await axios.get(error.config.url, error.config)).data;
-    }
-  }
+  const authAxios = getAuthAxios(access);
+  const result = await authAxios.get("/mypage");  // baseUrl 뒷 부분만
+  return result.data;
 };
