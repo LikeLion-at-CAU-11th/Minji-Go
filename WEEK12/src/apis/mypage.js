7ff5d1c34dfa29fe91267445fb0beefef7b0b5f8
gominzip <ahaminji@naver.com>
2023-07-11 19:08:34 +0900

    [CHORE] git 오류 수정

--- WEEK12/src/apis/mypage.js
+++
@@ -1,8 +0,0 @@
-import { getAuthAxios } from "./authAxios";
-// 데이터 요청
-export const getMyPage = async () => {
-  const access = localStorage.getItem("access"); // 로컬스토리지에서 access 토큰을 가져온다
-  const authAxios = getAuthAxios(access);
-  const result = await authAxios.get("/mypage");  // baseUrl 뒷 부분만
-  return result.data;
-};
