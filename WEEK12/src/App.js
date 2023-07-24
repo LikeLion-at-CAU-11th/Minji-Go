623b5fa3cfad112677a099bf504a044d822cf63f
gominzip <ahaminji@naver.com>
2023-07-25 02:33:52 +0900

    [FEAT] 과제 구현 완료

---
+++ WEEK12/src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import Mypage from "./pages/Mypage";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/signup" element={<SignUp/>}></Route>
+        <Route path="/mypage" element={<Mypage/>}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
